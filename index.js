c3ba576d188d9d32eb76958b6fc055c562b00c3c
BRAVO\tuanbd <bachdangtuan.dev@gmail.com>
2024-08-09 16:18:09 +0700

    update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 const app = express();
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }))
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs')
 app.use(express.json())
@@ -11,8 +11,8 @@ app.use(express.json())
 app.get("/", (req, res) => {
     res.send("Express on Vercel");
 });
-app.get("/check", (req, res) => {
-    res.render('index.ejs');
+app.get("/check", async (req, res) => {
+    res.render('index');
 });
 
 
