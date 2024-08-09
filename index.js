50045d72c0fd7b7152af6b02fea022bcd9799151
BRAVO\tuanbd <bachdangtuan.dev@gmail.com>
2024-08-09 16:25:01 +0700

    update

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const express = require("express");
 const axios = require('axios');
 const cheerio = require('cheerio');
 const app = express();
+const path = require('path')
+
 
 app.use(express.urlencoded({ extended: true }));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.get("/", async (req, res) => {
