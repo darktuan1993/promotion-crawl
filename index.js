d6eb7e7a4f87d2999ec2dacbd980ff5367dbc320
BRAVO\tuanbd <bachdangtuan.dev@gmail.com>
2024-08-09 16:14:56 +0700

    update

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const cheerio = require('cheerio');
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
-app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs')
+app.use(express.json())
 
 app.get("/", (req, res) => {
     res.send("Express on Vercel");
