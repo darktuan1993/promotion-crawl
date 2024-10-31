3b9ae21ebb99a7f2729f2d6ac61839a10ab144bd
BRAVO\tuanbd <bachdangtuan.dev@gmail.com>
2024-10-31 16:49:05 +0700

    update

--- setting/connectMongoDB.js
+++ setting/connectMongoDB.js
@@ -1,18 +1,18 @@
-const mongoose = require('mongoose');
-const mongoURI = 'mongodb+srv://bachdangtuandev:WYrA64rgtAfsdEWf@cluster0.6wyjf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+// const mongoose = require('mongoose');
+// const mongoURI = 'mongodb+srv://bachdangtuandev:WYrA64rgtAfsdEWf@cluster0.6wyjf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 
-// KẾT NỐI DATABASE MONGO
-const connectMongoDB = async () => {
-    try {
-        await mongoose.connect(mongoURI);
-        console.log(`### Kết nối thành công tới mongoDB Atlas ${mongoURI} `);
-    } catch (err) {
-        console.error('Lỗi kết nối MongoDB:', err.message);
-    }
-};
+// // KẾT NỐI DATABASE MONGO
+// const connectMongoDB = async () => {
+//     try {
+//         await mongoose.connect(mongoURI);
+//         console.log(`### Kết nối thành công tới mongoDB Atlas ${mongoURI} `);
+//     } catch (err) {
+//         console.error('Lỗi kết nối MongoDB:', err.message);
+//     }
+// };
 
 
-// Export Connect Mongo
-module.exports = {
-    connectMongoDB,
-};
+// // Export Connect Mongo
+// module.exports = {
+//     connectMongoDB,
+// };
